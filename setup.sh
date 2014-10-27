e2b5a8841c8186472afc5cddfe3fae7069ff6f43
Sangwoo Han <sangwoo.han00@gmail.com>
2014-10-27 11:38:25 +0900

    nvm and node to current latest version

--- setup.sh
+++ setup.sh
@@ -6,12 +6,12 @@
 # https://github.com/creationix/nvm
 sudo apt-get install -y git
 sudo apt-get install -y curl
-curl https://raw.githubusercontent.com/swhan00/setup/master/nvm-install.sh | sh
+curl https://raw.githubusercontent.com/creationix/nvm/v0.17.3/install.sh | bash
 
 # Load nvm and install latest production node
 source $HOME/.nvm/nvm.sh
-nvm install v0.10.12
-nvm use v0.10.12
+nvm install v0.10.32
+nvm use v0.10.32
 
 # Install jshint to allow checking of JS code within emacs
 # http://jshint.com/
@@ -25,7 +25,7 @@ sudo apt-get install -y rlwrap
 # https://launchpad.net/~cassou/+archive/emacs
 sudo add-apt-repository -y ppa:cassou/emacs
 sudo apt-get -qq update
-sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
+sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg emacs24
 
 # Install Heroku toolbelt
 # https://toolbelt.heroku.com/debian
